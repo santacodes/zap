b1b4e46563f56fa6bf99edc2ab83018fe9fd7d63
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-04-02 21:30:46 +0530

    added genotp module

---
+++ src/gen.rs
@@ -0,0 +1,5 @@
+fn genotp(email:&str,){
+    println!("{email}");
+    //do stuff to generate otp for that email
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod gen;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +7,15 @@ fn index() -> &'static str {
     "Welcome to Zap! A Superfast E-mail Verification API"
 }
 
+#[get("/info")]
+fn info() -> &'static str {
+    "This is the Info Page for Zap"
+    //gen::genotp();
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+    .mount("/info",routes![info])
+    
 }
