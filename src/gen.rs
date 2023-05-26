f2855515cdc815103f4411325143549e7dbfa5e1
Santhosh <52504160+santacodes@users.noreply.github.com>
2023-05-27 00:26:59 +0530

    added verification function

--- src/gen.rs
+++ src/gen.rs
@@ -4,8 +4,18 @@ pub fn genotp() -> i32 {
     //Generate and return a 4 digit number
     use rand::Rng; // 0.8.5
 
-    // Generate random number in the range [0, 99]
+    // Generate random number in the range [0, 99] to generate otp 
     let num = rand::thread_rng().gen_range(1000..9999);
     println!("This is the generated random number {}", num);
     return num;
 }
+ pub fn verify(otp: i32) -> bool {
+    let databaseotp = 2123;  //some random number for test
+    if(otp == databaseotp){
+        return true;
+
+    }else{
+        return false;
+    }
+    return false;
+ }
